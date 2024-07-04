9201aec847ab44390e3e52a5bc45c418bc7f14c9
Adam <f7f376a1fcd0d0e11a10ed1b6577c9@users.noreply.github.com>
2024-07-04 16:28:55 +0100

    feat: universal handler abstracted to new system

---
+++ lib/connmgr/universal.go
@@ -0,0 +1,106 @@
+package connmgr
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/nbd-wtf/go-nostr"
+)
+
+func SendUniversalEvent(ctx context.Context, connectionManager ConnectionManager, event *nostr.Event, subscriptionId *string) (map[string]*nostr.OKEnvelope, error) {
+	results := map[string]*nostr.OKEnvelope{}
+
+	for connectionID := range connectionManager.ListConnections() {
+		env, err := SendUniversalEventSingle(ctx, connectionManager, connectionID, event, subscriptionId)
+
+		if env != nil {
+			results[connectionID] = env
+		}
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to upload DAG to node %s: %w", connectionID, err)
+		}
+	}
+
+	return results, nil
+}
+
+func SendUniversalEventSingle(ctx context.Context, connectionManager ConnectionManager, connectionID string, event *nostr.Event, subscriptionId *string) (*nostr.OKEnvelope, error) {
+	stream, err := connectionManager.GetStream(ctx, connectionID, "/nostr/event/universal")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get stream for connection %s: %w", connectionID, err)
+	}
+	defer stream.Close()
+
+	streamEncoder := json.NewEncoder(stream)
+
+	reqMessage := nostr.EventEnvelope{
+		SubscriptionID: subscriptionId,
+		Event:          *event,
+	}
+
+	if err := streamEncoder.Encode(&reqMessage); err != nil {
+		return nil, err
+	}
+
+	response, err := ReadJsonMessageFromStream[[]json.RawMessage](stream)
+	if err != nil {
+		return nil, err
+	}
+
+	message := *response
+
+	if len(message) == 0 {
+		return nil, fmt.Errorf("empty message")
+	}
+	var typ string
+	if err = json.Unmarshal(message[0], &typ); err != nil {
+		return nil, err
+	}
+
+	switch typ {
+	case "NOTICE":
+		if len(message) != 2 {
+			return nil, fmt.Errorf("invalid notice message length: %d", len(message))
+		}
+
+		var m string
+		if err = json.Unmarshal(message[1], &m); err != nil {
+			return nil, err
+		}
+
+		return nil, fmt.Errorf(m)
+	case "OK":
+		if len(message) != 4 {
+			return nil, fmt.Errorf("invalid OK message length: %d", len(message))
+		}
+
+		var eventID string
+		if err = json.Unmarshal(message[1], &eventID); err != nil {
+			return nil, err
+		}
+		var ok bool
+		if err = json.Unmarshal(message[2], &ok); err != nil {
+			return nil, err
+		}
+		var m string
+		if err = json.Unmarshal(message[3], &m); err != nil {
+			return nil, err
+		}
+
+		if !ok {
+			return nil, fmt.Errorf(m)
+		}
+
+		okEnv := &nostr.OKEnvelope{
+			EventID: eventID,
+			OK:      ok,
+			Reason:  m,
+		}
+
+		return okEnv, nil
+	}
+
+	return nil, fmt.Errorf("invalid response")
+}
